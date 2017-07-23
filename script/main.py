d58fdb106b4ef86226f4d4a001a6144a68af9635
zhiyan <zhiyan1987@gmail.com>
2017-07-23 10:30:24 -0400

    'upload bug info - 1500820224.66'

---
+++ script/main.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+import sys,os,time,subprocess,random
+
+file_name = 'bug.md'
+file_path = '/root/www/github-record-script/'
+
+def check_file():
+	if not os.path.exists(file_path + file_name):
+		f = open(file_path + file_name,'w')
+		f.write(str(time.time()) + '\n')
+		f.close()
+
+def add_file_line():
+	f = open(file_path + file_name,'a+')
+	f.write(str(time.time()) + '\n')
+	f.close()
+
+def git_add():
+	cmd = ['git', 'add', '.']
+	p = subprocess.Popen(cmd,cwd=file_path)
+	print p.wait()
+
+def git_commit():
+	centext = "'upload bug info - " + str(time.time()) + "'"
+	cmd = ['git', 'commit', '-m',centext]
+	p = subprocess.Popen(cmd,cwd=file_path)
+	print p.wait()
+
+def git_push():
+	cmd = ['git', 'push', '-u','origin','master']
+	p = subprocess.Popen(cmd,cwd=file_path)
+	print p.wait()
+
+def cmd_list():
+	cmd_list = []
+	cmd_list.append("git add .")
+	cmd_list.append("git commit -m 'upload bug info - " + str(time.time()) + "'")
+	cmd_list.append("git push -u origin master")
+	return cmd_list
+	
+def cmd_go():
+	cmd_str_list = cmd_list()
+	for cmd_go in cmd_str_list:
+		subprocess.call(cmd_go,shell=True)
+		
+def file_handle():
+	check_file()
+	add_file_line()
+	git_add()
+	git_commit()
+	git_push()
+
+if __name__ == "__main__":
+	file_handle()	
+	print 'done.'
